9fde7d1ba84eddeb39e110d9f0e764d2b99cde25
fish <chx09129@yahoo.com.tw>
2017-09-21 15:46:05 +0800

    out num

--- test/main.cpp
+++ test/main.cpp
@@ -1,41 +1,9 @@
-#include "math.h"
+
 #include <iostream>
 #include <memory>
 using namespace std;
 int main() {
-    SafeArray arrs[] = {SafeArray(5), SafeArray(10), SafeArray(15)};
     
-    for(int i = 0; i < 3; i++) {
-        SafeArray *safeArray_ptr = arrs ;
-        safeArray_ptr+=i;
-        for(int j = 0; j < safeArray_ptr->length; j++) {
-            safeArray_ptr->set(j, (j + 1) * 10);
-        }
-    }
-    
-    for(int i = 0; i < 3; i++) {
-        SafeArray *safeArray_ptr = arrs + i;
-        for(int j = 0; j < safeArray_ptr->length; j++) {
-            cout << safeArray_ptr->get(j) << " ";
-        }
-        cout << endl;
-    }
-    cout << endl<<endl;
-    for(int i = 0; i < 3; i++) {
-        
-        for(int j = 0; j < arrs[i].length; j++) {
-            arrs[i].set(j, (j + 1) * 10);
-            
-        }
-    }
-    for(int i = 0; i < 3; i++) {
-        
-        for(int j = 0; j < arrs[i].length; j++) {
-            cout <<arrs[i].get(j)<< " ";
-            
-        }
-        cout << endl;
-    }
-    cout<<endl<<SafeArray::PI<<endl;
+    cout<<endl<<"23233232"<<endl;
     return 0;
 }
